60f3b1af3795296cce85c80b8907d440291b0192
Ines Escobedo <inesescobedo@dhcp-10-29-138-69.dyn.MIT.EDU>
2023-10-03 15:23:38 -0400

    1.2

--- condition_boxplots.py
+++ condition_boxplots.py
@@ -9,20 +9,24 @@ df = pd.read_excel('condition_counts1.xlsx')
 condition_columns = df.columns[2:]
 
 # Filter out languages with less than 32 members and not equal to "Total"
-df_filtered = df[(df['PRIMARYLANGUAGE_COUNT'] >= 32)]
+df_filtered = df[(df['PRIMARYLANGUAGE_COUNT'] >= 2000)]
+for condition in condition_columns:
+    df_filtered[f'% with {condition}'] = (df_filtered[condition] / df_filtered['PRIMARYLANGUAGE_COUNT']) * 100
 
 # Perform calculations and create box plots for each condition
 for condition in condition_columns:
+    
+
     # Filter the DataFrame for "Other Languages" and English speakers for each condition
-    english_speakers_condition = df_filtered.loc[df_filtered['PRIMARYLANGUAGE'] == 'ENGLISH', condition]
+    english_speakers_condition = df_filtered.loc[df_filtered['PRIMARYLANGUAGE'] == 'ENGLISH', f'% with {condition}']
     print(english_speakers_condition)
-    other_languages_condition = df_filtered.loc[df_filtered['PRIMARYLANGUAGE'] != 'ENGLISH', condition]
+    other_languages_condition = df_filtered.loc[df_filtered['PRIMARYLANGUAGE'] != 'ENGLISH', f'% with {condition}']
 
     sns.boxplot(data=[other_languages_condition, english_speakers_condition],
                 flierprops=dict(marker='o', markersize=5, markerfacecolor='red', linestyle='none'),
                 color="#253532")
     plt.xlabel('Language Group')
-    plt.ylabel(condition)
+    plt.ylabel(f'% with {condition}')
     plt.title(f'Distribution of {condition} %: English Speakers vs. LEP')
     plt.xticks([0, 1], ['Other Languages', 'English Speakers'])
 
